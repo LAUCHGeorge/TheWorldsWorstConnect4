321f97533151e6d873ebe2e97079ff501e43cedd
lauchgeorge <laubholdgeorg@gmail.com>
2024-04-04 21:07:02 +0200

    UI Working + name change to "The worlds worst connect 4".
    UI Class pretty much done, but still subject to change due to the prototype like UI.
    Name was translated to english from "The worlds worst 4 gewinnt" to "The worlds worst connect 4".

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
     private static UI ui;
     public static void main(String[] args) {
 
+
+
+        int turn = 0;
+
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
@@ -21,5 +25,47 @@ public class Main {
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setContentPane(ui.getMainPanel());
         mainFrame.setVisible(true);
+
+
+    }
+
+    public static void buttonPress(int player,int[][] grid,int column) {
+        boolean valid = true;
+        int[] panelxy = {-1,-1};
+
+        if (grid[0][column] == -1) {
+            grid[0][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 0;
+        } else if (grid[1][column] == -1) {
+            grid[1][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 1;
+        } else if (grid[2][column] == -1) {
+            grid[2][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 2;
+        } else if (grid[3][column] == -1) {
+            grid[3][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 3;
+        } else if (grid[4][column] == -1) {
+            grid[4][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 4;
+        } else if (grid[5][column] == -1) {
+            grid[5][column] = player;
+            panelxy[0] = column;
+            panelxy[1] = 5;
+        } else {
+            valid = false;
+        }
+
+
+        if (valid) {
+            ui.setPanelColor(Color.RED,panelxy);
+            ui.transferGrid(grid);
+        }
+
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UI extends Main{
     private JPanel mainPanel;
@@ -58,12 +60,231 @@ public class UI extends Main{
     private JPanel panel50;
     private JPanel panel60;
 
+    private JPanel panel;
+
+    public UI(JPanel panel) {
+        this.panel = panel;
+    }
+
+
+    public UI() {
+
+        int test = 0;
+        Main main = new Main();
+        int[][] grid = {
+                {-1,-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1,-1},
+                {-1,-1,-1,-1,-1,-1,-1}
+        };
+
+        initializeActionListener(grid,button0,0);
+        initializeActionListener(grid,button1,1);
+        initializeActionListener(grid,button2,2);
+        initializeActionListener(grid,button3,3);
+        initializeActionListener(grid,button4,4);
+        initializeActionListener(grid,button5,5);
+        initializeActionListener(grid,button6,6);
+
+    }
+
     public JPanel getMainPanel(){
 
         return this.mainPanel;
     }
-    public void setPanelColor(Color color){
-        panel00.setBackground(color);
+
+    private void initializeActionListener(int[][] grid,JButton button,int column) {
+
+        for( ActionListener al : button.getActionListeners() ) {
+                button.removeActionListener( al );
+        }
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.buttonPress(1,grid,column);
+            }
+        });
+    }
+
+    public void transferGrid(int[][] grid) {
+        initializeActionListener(grid,button0,0);
+        initializeActionListener(grid,button1,1);
+        initializeActionListener(grid,button2,2);
+        initializeActionListener(grid,button3,3);
+        initializeActionListener(grid,button4,4);
+        initializeActionListener(grid,button5,5);
+        initializeActionListener(grid,button6,6);
+    }
+    public void setPanelColor(Color color,int[] panelxy){
+        JPanel panel = null;
+        switch(panelxy[0]) {
+            case 0:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel00;
+                        break;
+                    case 1:
+                        panel = panel01;
+                        break;
+                    case 2:
+                        panel = panel02;
+                        break;
+                    case 3:
+                        panel = panel03;
+                        break;
+                    case 4:
+                        panel = panel04;
+                        break;
+                    case 5:
+                        panel = panel05;
+                        break;
+                        
+                }
+                break;
+            case 1:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel10;
+                        break;
+                    case 1:
+                        panel = panel11;
+                        break;
+                    case 2:
+                        panel = panel12;
+                        break;
+                    case 3:
+                        panel = panel13;
+                        break;
+                    case 4:
+                        panel = panel14;
+                        break;
+                    case 5:
+                        panel = panel15;
+                        break;
+
+                }
+                break;
+            case 2:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel20;
+                        break;
+                    case 1:
+                        panel = panel21;
+                        break;
+                    case 2:
+                        panel = panel22;
+                        break;
+                    case 3:
+                        panel = panel23;
+                        break;
+                    case 4:
+                        panel = panel24;
+                        break;
+                    case 5:
+                        panel = panel25;
+                        break;
+
+                }
+                break;
+            case 3:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel30;
+                        break;
+                    case 1:
+                        panel = panel31;
+                        break;
+                    case 2:
+                        panel = panel32;
+                        break;
+                    case 3:
+                        panel = panel33;
+                        break;
+                    case 4:
+                        panel = panel34;
+                        break;
+                    case 5:
+                        panel = panel35;
+                        break;
+
+                }
+                break;
+            case 4:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel40;
+                        break;
+                    case 1:
+                        panel = panel41;
+                        break;
+                    case 2:
+                        panel = panel42;
+                        break;
+                    case 3:
+                        panel = panel43;
+                        break;
+                    case 4:
+                        panel = panel44;
+                        break;
+                    case 5:
+                        panel = panel45;
+                        break;
+
+                }
+                break;
+            case 5:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel50;
+                        break;
+                    case 1:
+                        panel = panel51;
+                        break;
+                    case 2:
+                        panel = panel52;
+                        break;
+                    case 3:
+                        panel = panel53;
+                        break;
+                    case 4:
+                        panel = panel54;
+                        break;
+                    case 5:
+                        panel = panel55;
+                        break;
+
+                }
+                break;
+            case 6:
+                switch(panelxy[1]) {
+                    case 0:
+                        panel = panel60;
+                        break;
+                    case 1:
+                        panel = panel61;
+                        break;
+                    case 2:
+                        panel = panel62;
+                        break;
+                    case 3:
+                        panel = panel63;
+                        break;
+                    case 4:
+                        panel = panel64;
+                        break;
+                    case 5:
+                        panel = panel65;
+                        break;
+
+                }
+                break;    
+        }
+        
+        
+        panel.setBackground(color);
     }
 
 }
