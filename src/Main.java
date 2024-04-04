11b45f2a2346bcdbf8025e5090eac2eca5c465ba
Laubhold Georg <laubgeor@bgbrgwels.at>
2024-04-04 14:54:01 +0200

    Added simple prototype UI. No code in the project as of now.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,59 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(500,500);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
+    private JPanel mainPanel;
+    private JPanel titleContainer;
+    private JPanel gameContainer;
+    private JPanel selectorPanel;
+    private JPanel displayPanel;
+    private JButton button0;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JLabel titleLabel;
+    private JPanel panel05;
+    private JPanel panel00;
+    private JPanel panel01;
+    private JPanel panel02;
+    private JPanel panel03;
+    private JPanel panel04;
+    private JPanel panel12;
+    private JPanel panel15;
+    private JPanel panel14;
+    private JPanel panel13;
+    private JPanel panel21;
+    private JPanel panel31;
+    private JPanel panel41;
+    private JPanel panel51;
+    private JPanel panel61;
+    private JPanel panel11;
+    private JPanel panel22;
+    private JPanel panel32;
+    private JPanel panel23;
+    private JPanel panel24;
+    private JPanel panel25;
+    private JPanel panel35;
+    private JPanel panel34;
+    private JPanel panel33;
+    private JPanel panel45;
+    private JPanel panel44;
+    private JPanel panel55;
+    private JPanel panel65;
+    private JPanel panel64;
+    private JPanel panel54;
+    private JPanel panel63;
+    private JPanel panel53;
+    private JPanel panel43;
+    private JPanel panel42;
+    private JPanel panel52;
+    private JPanel panel62;
+    private JPanel panel10;
+    private JPanel panel20;
+    private JPanel panel30;
+    private JPanel panel40;
+    private JPanel panel50;
+    private JPanel panel60;
 }
