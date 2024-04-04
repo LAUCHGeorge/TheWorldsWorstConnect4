2947a80f84e56b90f7cb7c51f1c5911fe0fb4f24
Laubhold Georg <laubgeor@bgbrgwels.at>
2024-04-04 14:02:55 +0200

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
